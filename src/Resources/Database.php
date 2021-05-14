700371461242c8a2a307d1394f98c5a8cc05fb62
Daniel Vigueras <dviguerasc@gmail.com>
2021-05-14 11:49:59 +0200

    Add code to retrieve a Notion Database

---
+++ src/Resources/Database.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace R64\PhpNotion\Resources;
+
+class Database
+{
+    private string $object;
+    private string $id;
+    private string $createdTime;
+    private string $lastEditedTime;
+    private array $title;
+    private object $properties;
+
+    public function __construct(
+        string $object,
+        string $id,
+        string $createdTime,
+        string $lastEditedTime,
+        array $title,
+        object $properties
+    ) {
+        $this->object = $object;
+        $this->id = $id;
+        $this->createdTime = $createdTime;
+        $this->lastEditedTime = $lastEditedTime;
+        $this->title = $title;
+        $this->properties = $properties;
+    }
+
+    public function object(): string
+    {
+        return $this->object;
+    }
+
+    public function id(): string
+    {
+        return $this->id;
+    }
+
+    public function createdTime(): string
+    {
+        return $this->createdTime;
+    }
+
+    public function lastEditedTime(): string
+    {
+        return $this->lastEditedTime;
+    }
+
+    public function title(): array
+    {
+        return $this->title;
+    }
+
+    public function properties(): object
+    {
+        return $this->properties;
+    }
+}
---
+++ src/Resources/Databases.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace R64\PhpNotion\Resources;
+
+use R64\PhpNotion\Integrations\NotionClient;
+
+class Databases
+{
+    private NotionClient $notionClient;
+
+    public function __construct(NotionClient $notionClient)
+    {
+        $this->notionClient = $notionClient;
+    }
+
+    public function retrieve(string $databaseId): Database
+    {
+        $response = $this->notionClient->getResource('databases', $databaseId);
+
+        return new Database(
+            $response->object,
+            $response->id,
+            $response->created_time,
+            $response->last_edited_time,
+            $response->title,
+            $response->properties
+        );
+    }
+}
